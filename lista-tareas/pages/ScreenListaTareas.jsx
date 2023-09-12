3c39072c19e1cb1dd27803f35ed5968702515b42
Aluon <paulacanoroman@gmail.com>
2023-09-12 21:24:56 +0200

    esqueleto de divs

---
+++ lista-tareas/pages/ScreenListaTareas.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ScreenListaTareas = () => {
+  return (
+    <>
+    <div>ScreenListaTareas</div>
+    <div>input + boton añadir</div>
+    <div>Tareas agregadas con boton eliminar</div>
+    </>
+  )
+}
+
+export default ScreenListaTareas
